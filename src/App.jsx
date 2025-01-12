3428830baa0a1598646e7010b7cd60ab9340b494
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-01-12 17:40:21 +0500

    add navbar and assets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./sections/Navbar";
+
 export default function App() {
   return (
-    <>
-    </>
+    <main className="max-w-7xl mx-auto">
+      <Navbar/>
+    </main>
   )
 }
